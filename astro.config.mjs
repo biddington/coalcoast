1f6b76ed13684b95fe3a09f5f495cc7bdf294a1a
mrgoldair <timbiddington@fastmail.com.au>
2024-05-08 22:18:07 +1000

    Cohesion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
-
 import react from "@astrojs/react";
+
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react()]
+  integrations: [expressiveCode(), mdx(), react(), ]
 });
